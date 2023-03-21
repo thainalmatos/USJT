da951c6baadc4b62cef13a39e53b7f8742df6bb1
Thaina Lima <thaina.matos0504@gmail.com>
2023-03-20 21:52:05 -0300

    aula 23-3, organizacao de pastas e hello world

---
+++ Aulas/Março/20-03-23/Mostra1a100.java
@@ -0,0 +1,11 @@
+public class Mostra1a100 {
+    public static void main (String args[]){
+        System.out.println("Oii, sei contar de 1 a 100: ");
+        int count = 1;
+        while (count <= 100){
+            System.out.print(count + ", ");
+            count = count + 1;
+        }
+
+    }
+}
